36f486ea5ca2f6bfcf2b410f510e061b021ebd96
kkoma210 <kkoma@sparcs.org>
2021-05-29 21:20:28 +0900

    Add navigation bar

--- babpull/src/App.js
+++ babpull/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NavBar from "./myComponent/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <h1>밥풀</h1>
+        </Route>
+        {/* <Route exact path="/counter">
+          <CounterPage />
+        </Route>
+        <Route exact path="/dogs">
+          <DogPage />
+        </Route>
+        <Route exact path="/todo">
+          <TodoPage />
+        </Route> */}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
