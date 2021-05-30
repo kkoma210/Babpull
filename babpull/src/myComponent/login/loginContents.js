f2ecf8e3ff2a74a85ad41a07285d6eef5797ddac
kkoma210 <kkoma@sparcs.org>
2021-05-30 23:21:16 +0900

    Add onLoginClick

--- babpull/src/myComponent/login/loginContents.js
+++ babpull/src/myComponent/login/loginContents.js
@@ -2,20 +2,38 @@ import AuthContent from "./AuthContents";
 import InputWithLabel from "./inputLabel";
 import AuthButton from "./authButton";
 import RightLink from "./rightLink";
-import RegisterPage from "./registerPage";
 import React, {useState} from "react";
+import axios from "axios"
 
 const LoginContent = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    }
 
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
+
+    const onLoginClick = () => {
+        // 추가 요청 전송
+        console.log("login click");
+        axios.post(`/api/login`, {
+          id: email,
+          password: password
+        })
+        // 완료 후 목록 조회 요청 전송
+        // .then(() => axios.get(`/api/todo`))
+        // 응답이 돌아오면 응답 내용으로 목록을 변경
+      };
 
     return(
         <AuthContent title="로그인">
-            <InputWithLabel label="이메일" name="email" placeholder="이메일" value={email}/>
-            <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password" value={password}/>
-            <AuthButton>로그인</AuthButton>
+            <InputWithLabel label="이메일" name="email" placeholder="이메일" value={email} onChange={onEmailHandler}/>
+            <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password" value={password} onChange={onPasswordHandler}/>
+            <AuthButton onClick={() => onLoginClick()}>로그인</AuthButton>
             <RightLink to="/register">회원가입</RightLink>
         </AuthContent>
     );
--- babpull/src/myComponent/login/registerPage.js
+++ babpull/src/myComponent/login/registerPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import AuthWrapper from "./loginWrapper";
 import RegisterContent from "./registerContents";
 
