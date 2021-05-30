e0c6874012e6bb337cb4dc36375c76062a0599b8
kkoma210 <kkoma@sparcs.org>
2021-05-30 22:37:13 +0900

    Add body parser

--- babpull_server/routes/login.js
+++ babpull_server/routes/login.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../db");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login', { title: 'login' });
+router.post('/', (req, res) => {
+    
 });
 
 module.exports = router;
